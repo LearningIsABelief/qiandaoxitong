d83bb336ca31ba63e6c3360a5ef0c3f126522ec0
zhandongyang <zhan19103768615@163.com>
2022-05-10 11:31:29 +0800

    将已签到模型从checkin.go中独立出来

--- model/checkin.go
+++ model/checkin.go
@@ -16,15 +16,3 @@ type Checkin struct {
 	UpdatedAt time.Time  `gorm:"updated_at"`
 	DeletedAt *time.Time `gorm:"deleted_at"`
 }
-
-type CheckedIn struct {
-	ID        string `json:"id" gorm:"primaryKey"`
-	CheckinID string `json:"checkin_id" sql:"index"`
-	UserID    string `json:"user_id" sql:"index"`
-	UserName  string `json:"user_name"`
-	State     bool   `json:"state"`
-
-	CreatedAt time.Time  `gorm:"created_at"`
-	UpdatedAt time.Time  `gorm:"updated_at"`
-	DeletedAt *time.Time `gorm:"deleted_at"`
-}
