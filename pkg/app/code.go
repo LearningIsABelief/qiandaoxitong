656d11c355b3fc3a8250a4dbebbc0837ffc1a5ee
yangxuzheng <1391625461@qq.com>
2022-05-14 14:30:30 +0800

    修改登录密码错误状态码

--- pkg/app/code.go
+++ pkg/app/code.go
@@ -5,14 +5,14 @@ var (
 	OK                  = &Errno{Code: 200, Message: "OK"}
 	InternalServerError = &Errno{Code: 500, Message: "服务器异常."}
 	ErrBind             = &Errno{Code: 10002, Message: "将请求正文绑定到结构时发生错误."}
-	ErrParamNull = &Errno{Code: 11001, Message: "参数结果为空"}
+	ErrParamNull        = &Errno{Code: 11001, Message: "参数结果为空"}
 
 	// 课程错误
-	ErrLessonExist = &Errno{Code: 11004,Message: "课程已存在"}
-	ErrRecordNotExist = &Errno{Code:11005,Message: "查询记录不存在"}
-	ErrUpdated = &Errno{Code:11006,Message: "更新失败"}
-	ErrInserted = &Errno{Code:11007,Message: "插入失败"}
-	ErrDeleted = &Errno{Code:11008,Message: "删除失败"}
+	ErrLessonExist    = &Errno{Code: 11004, Message: "课程已存在"}
+	ErrRecordNotExist = &Errno{Code: 11005, Message: "查询记录不存在"}
+	ErrUpdated        = &Errno{Code: 11006, Message: "更新失败"}
+	ErrInserted       = &Errno{Code: 11007, Message: "插入失败"}
+	ErrDeleted        = &Errno{Code: 11008, Message: "删除失败"}
 
 	// 用户错误
 	ErrEncrypt             = &Errno{Code: 20101, Message: "加密用户密码时出错"}
@@ -24,7 +24,7 @@ var (
 	ErrPhoneDoesNotExist   = &Errno{Code: 20107, Message: "手机号不存在"}
 	ErrPhoneBinEmail       = &Errno{Code: 20108, Message: "请输入手机号绑定的正确邮箱"}
 	ErrAccountDoesNotExist = &Errno{Code: 20109, Message: "账号不存在"}
-	ErrLoginPassword       = &Errno{Code: 20105, Message: "密码错误"}
+	ErrLoginPassword       = &Errno{Code: 20110, Message: "密码错误"}
 	ErrUserNotExist        = &Errno{Code: 11002, Message: "用户不存在"}
 
 	// token
